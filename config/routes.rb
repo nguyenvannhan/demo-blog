26f767bdd547d92679c18a7158bbf47a323d551b
nvnhan <nguyenvannhan0810@gmail.com>
2022-12-01 21:21:35 +0700

    Prepare routes create - edit - delete comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :blogs
+  resources :blogs do
+    resources :comments, shallow: true, only: [:create, :update, :destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
