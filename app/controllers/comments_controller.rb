a57f5df89c8bcb1e5b78ca2f5e4eb072f2148e62
nvnhan <nguyenvannhan0810@gmail.com>
2022-12-02 00:40:54 +0700

    Update comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[edit update destroy]
 
-  def create 
+  def create
     comment_params = params.require(:comment).permit(:title, :content, :blog_id)
 
     @comment = Comment.new(comment_params)
@@ -9,7 +10,26 @@ class CommentsController < ApplicationController
     @comment.blog = @blog
     @comment.save
 
-    redirect_to blog_url(params[:blog_id]), notice: "Create comment successfully"
+    redirect_to blog_url(params[:blog_id]), notice: 'Create comment successfully'
   end
 
+  def edit
+    @blog = @comment.blog
+  end
+
+  def update
+    comment_params = params.require(:comment).permit(:title, :content)
+
+    if @comment.update(comment_params)
+      redirect_to blog_url(@comment.blog_id), notice: 'Update Comment successfully'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :blogs do
-    resources :comments, shallow: true, only: [:create, :update, :destroy]
+    resources :comments, shallow: true, only: [:create, :edit, :update, :destroy]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
